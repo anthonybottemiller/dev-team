91b67913cb75157acf3532969571d382e5dfcca4
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 13:40:50 -0800

    implement specialty pipe

--- src/app/specialty.pipe.ts
+++ src/app/specialty.pipe.ts
@@ -1,12 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { FirebaseListObservable } from 'angularfire2';
+import { Teammate } from './teammate.model';
 @Pipe({
   name: 'specialty'
 })
 export class SpecialtyPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: Teammate[], specialty) {
+    var output: Teammate[] = [];
+    if(specialty === "Front-End") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].specialty === "Front-End") {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (specialty === "Back-End") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].specialty === "Back-End") {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
+    }
   }
-
 }
