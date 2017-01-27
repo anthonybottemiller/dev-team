c5a0a39f535514f8b12418da27343ea49be518cc
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 09:26:11 -0800

    import firebase config and initialize for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
+import { AngularFireModule } from 'angularfire2';
+import { masterFirebaseConfig } from './api-keys';
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
