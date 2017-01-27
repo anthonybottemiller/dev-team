1ab1cf12abd77c9c410492860885345c5ef1afcd
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 12:44:44 -0800

    implement required code to display teammate referenced by dynamic route

--- src/app/teammate-detail/teammate-detail.component.ts
+++ src/app/teammate-detail/teammate-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TeammateService } from '../teammate.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Teammate } from '../teammate.model';
+import { FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-teammate-detail',
   templateUrl: './teammate-detail.component.html',
-  styleUrls: ['./teammate-detail.component.css']
+  styleUrls: ['./teammate-detail.component.css'],
+  providers: [TeammateService]
 })
 export class TeammateDetailComponent implements OnInit {
+  teammateId: string = null;
+  teammateToDisplay;ß
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private teammateService: TeammateService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.teammateId = urlParameters['id'];
+    });
+    this.teammateToDisplay = this.teammateService.getTeammateById(this.teammateId);
   }
 
 }
