eee13c3de0181fd8a592efb7dcb813ddd0117fa6
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 14:21:04 -0800

    implement admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Teammate } from '../teammate.model';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { TeammateService } from '../teammate.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [TeammateService]
 })
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private teammateService: TeammateService) { }
 
   ngOnInit() {
   }
 
+  submitForm(name: string, specialty: string, email: string, phone: string) {
+    var newTeammate: Teammate = new Teammate(name, specialty, phone, email);
+    this.teammateService.addTeammate(newTeammate);
+  }
 }
