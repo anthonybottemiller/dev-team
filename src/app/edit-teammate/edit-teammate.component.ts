c39ab1211a460690a5bfac157311a4faccfb2a56
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 15:07:43 -0800

    add beginUpdatingTeammate() method for click event

--- src/app/edit-teammate/edit-teammate.component.ts
+++ src/app/edit-teammate/edit-teammate.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Teammate } from '../teammate.model';
+import { TeammateService } from '../teammate.service';
 
 @Component({
   selector: 'app-edit-teammate',
   templateUrl: './edit-teammate.component.html',
-  styleUrls: ['./edit-teammate.component.css']
+  styleUrls: ['./edit-teammate.component.css'],
+  providers: [TeammateService]
 })
-export class EditTeammateComponent implements OnInit {
 
-  constructor() { }
+export class EditTeammateComponent implements OnInit {
+  @Input() selectedTeammate;
+
+  constructor(private teammateService: TeammateService) { }
 
   ngOnInit() {
   }
 
+  beginUpdatingTeammate(teammateToUpdate){
+    this.teammateService.updateTeammate(teammateToUpdate);
+  }
 }
