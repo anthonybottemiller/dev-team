d1973ce1f759413e9849d055a11f6908c230b5ec
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 13:41:28 -0800

    add onChange() method

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 export class TeamListComponent implements OnInit {
 
   teammates: FirebaseListObservable<any[]>;
+  filterBySpecialty: string = "All-Devs";
 
   constructor(private router: Router, private teammateService: TeammateService) { }
 
@@ -23,5 +24,9 @@ export class TeamListComponent implements OnInit {
 
   goToDetail(clickedTeammate) {
     this.router.navigate(['teammates', clickedTeammate.$key]);
+  }
+
+  onChange(optionFromMenu) {
+    this.filterBySpecialty = optionFromMenu;
   }
 }
