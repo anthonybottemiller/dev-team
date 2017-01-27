3e697e91d1dd0828a61cc48b03e31605f0056597
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 10:47:20 -0800

    add code to use TeammateService

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Teammate } from '../teammate.model';
 import { TeammateService } from '../teammate.service'
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+  styleUrls: ['./team-list.component.css'],
+  providers: [TeammateService]
 })
+
 export class TeamListComponent implements OnInit {
 
-  constructor() { }
+  teammates: FirebaseListObservable<any[]>;
+
+  constructor(private teammateService: TeammateService) { }
 
   ngOnInit() {
+    this.teammates = this.teammateService.getTeammates();
   }
 
 }
