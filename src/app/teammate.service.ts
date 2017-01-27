1be3574dc3da70e061abd9d02687ead23c224ad9
Anthony Bottemiller <anthony@Anvil.local>
2017-01-27 10:03:00 -0800

    implement getTeamates() for teammate service

--- src/app/teammate.service.ts
+++ src/app/teammate.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Teammate } from './teammate.model';
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Injectable()
 export class TeammateService {
+  teammates: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+  this.teammates = angularFire.database.list('teammates');
+  }
 
+  getTeammates(){
+    return this.teammates;
+  }
 }
